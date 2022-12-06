0e837f6a90c02938d5ffa3fa6bdd167bf684d35a
Jovanovic <74651542+JovanoMat@users.noreply.github.com>
2022-12-06 19:56:56 +0100

    Day 3 completed!

--- day3.py
+++ day3.py
@@ -1,5 +1,5 @@
-# testString = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw"
-# rucksacks = testString.split("\n")
+#testString = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw"
+#rucksacks = testString.split("\n")
 
 # get input via file and save as string
 file = open("day3input.txt", "r");
@@ -44,8 +44,36 @@ for rucksack in rucksacks:
 
 print("The total priority comes out to" , totalPrio, "points!")
 
+# Reset all variables
+totalPrio = 0;
+rucksackCount = 0;
+currentGroup = list();
+foundInGroup = False;
+
+# check each rucksack
+for rucksack in rucksacks:
+    # add a new rucksack to the current group, if the group doesn't have 3 rucksacks yet
+    currentGroup.append(rucksack)
+    if len(currentGroup) != 3:
+        continue;
+    else:
+        # check every letter
+        for letterfir in currentGroup[0]:
+            for lettersec in currentGroup[1]:
+                for letterthi in currentGroup[2]:
+                    # if a same letter in all the rucksacks has been found, add the prio to the total prio
+                    if letterfir == lettersec == letterthi and foundInGroup == False:
+                        foundInGroup = True
+
+                        posLetter = letters.index(letterfir)
+                        totalPrio += prios[posLetter] 
+                    
+        # reset the current group and the foundInGroup variable
+        currentGroup = list();
+        foundInGroup = False;
 
 
+print("The total priority, when looking at groups of three rucksacks, comes out to" , totalPrio, "points!")
 
 
 
