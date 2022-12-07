d252c0d01cfc1835e1c985f0c908808eb072911e
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-12-07 18:15:22 +0100

    day 4 completed!

--- day4.py
+++ day4.py
@@ -10,15 +10,59 @@ file.close()
 # 2-6,4-8'''.split("\n")
 
 count = 0;
+# check every elve-pairs assignment
 for assignment in assignments:
+
+    # split that assignments two sectors in a list
     firstsector, secondsector = assignment.split(",")
 
-    #auch doppelte zahlen
-    if int(firstsector[0]) > int(secondsector[0]) and int(firstsector[2]) <= int(secondsector[2]) or int(firstsector[0]) < int(secondsector[0]) and int(firstsector[2]) >= int(secondsector[2]):
+    #splitting each sector range into only the numbers
+    firstsector = firstsector.split("-")
+    secondsector = secondsector.split("-")
+
+    # get the start and end sector of the first range
+    firstSecFNum = int(firstsector[0])
+    firstSecLNum = int(firstsector[1])
+
+    # get the start and end sector of the second range
+    secondSecFNum = int(secondsector[0])
+    secondSecLNum = int(secondsector[1])
+
+    # checking
+    if firstSecFNum >= secondSecFNum and firstSecLNum <= secondSecLNum \
+        or firstSecFNum <= secondSecFNum and firstSecLNum >= secondSecLNum:
+
         count += 1
 
-print(count) 
-    
+print("A total of ", count, "sectors are being completely overlapped by their other half in the pair!")
+
+
+count = 0;
+# check every elve-pairs assignment
+for assignment in assignments:
+
+    # split that assignments two sectors in a list
+    firstsector, secondsector = assignment.split(",")
+
+    #splitting each sector range into only the numbers
+    firstsector = firstsector.split("-")
+    secondsector = secondsector.split("-")
+
+    # get the start and end sector of the first range
+    firstSecFNum = int(firstsector[0])
+    firstSecLNum = int(firstsector[1])
+
+    # get the start and end sector of the second range
+    secondSecFNum = int(secondsector[0])
+    secondSecLNum = int(secondsector[1])
+
+    # checking
+    if firstSecLNum >= secondSecFNum and secondSecLNum >= firstSecFNum:
+        count += 1
+
+
+print("There are a total of ", count, "overlaps of assgined sectors in every assignment!")
+
 
 
 
