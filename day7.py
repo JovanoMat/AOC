c24e8e8c2be85dfe281d72ed71fd4ad1c91da129
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-12-12 16:25:04 +0100

    day 7 started

---
+++ day7.py
@@ -0,0 +1,51 @@
+
+import re
+
+
+commands = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k""".split("\n")
+
+
+sizeByDir = list()
+
+dirs = list()
+
+for command in commands:
+    if "cd" in command:
+        currentDir = command.split(" ")[2]
+        
+        if currentDir == "..":
+            
+            print("Not implemented")
+
+    
+        currentSum = 0
+
+    if re.search(r"^\d", command):
+        currentSum += int(command.split(" ")[0])            
+        print(currentSum)            
+
+
+
+
